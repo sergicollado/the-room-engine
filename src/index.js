c2163673d46963b9777e5579251cbd4dba70c54c
Sergi Collado <sergio.collado@soprasteria.com>
2022-11-19 10:01:28 +0100

    feat(package): exports actionTypes and responseDefinitions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 const {TheRoomEngine} = require("./domain/engine");
+const {ActionType} = require("./domain/actions");
+const {ResponseDefinition} = require("./domain/responseDefinition");
 
 exports.TheRoomEngine = TheRoomEngine;
+exports.ActionType = ActionType;
+exports.ResponseDefinition = ResponseDefinition;
