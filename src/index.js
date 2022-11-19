ad53f2cb4c1f37a670d116bbe7463ac24e46318e
Sergi Collado <sergio.collado@soprasteria.com>
2022-11-19 11:59:36 +0100

    feat(engine): exports Feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const {TheRoomEngine} = require("./domain/engine");
 const {ActionType} = require("./domain/actions");
 const {ResponseDefinition} = require("./domain/responseDefinition");
+const {Feature} = require("./domain//interactiveObjects");
 
 exports.TheRoomEngine = TheRoomEngine;
 exports.ActionType = ActionType;
 exports.ResponseDefinition = ResponseDefinition;
+exports.Feature = Feature;
